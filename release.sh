4a4f1b0cacb6abfcec7a112da3edb902a18ea77c
李梦克 <limengke@youzan.com>
2021-07-14 22:53:54 +0800

    chore: 安装依赖，增加配置文件

---
+++ release.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # build
+  npm version $VERSION --message "[release] $VERSION"
+  npm run build
+
+  # publish
+  BRANCH=$(git symbolic-ref --short HEAD)
+
+  git tag v$VERSION
+  git commit -am "[release] $VERSION"
+
+  git push origin $BRANCH
+  if [ $BRANCH == 'master' ]
+  then
+    echo "Add tag v$VERSION"
+    git push origin refs/tags/v$VERSION
+  fi
+
+  if [[ $VERSION =~ [beta] ]]
+  then
+    npm publish --registry=http://registry.npm.qima-inc.com --tag beta
+  elif [[ $VERSION =~ [rc] ]]
+  then
+    npm publish --registry=http://registry.npm.qima-inc.com --tag release
+  else
+    npm publish --registry=http://registry.npm.qima-inc.com
+  fi
+fi
